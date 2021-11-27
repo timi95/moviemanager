bb1c9c60e8bee8e4ec01448bd348a16ee6610791
Timi <timi.ogunkeye@gmail.com>
2021-11-27 15:22:45 +0000

    holly woo

--- src/main/java/com/robotech/magellan/moviemanager/controllers/HollyWooController.java
+++ src/main/java/com/robotech/magellan/moviemanager/controllers/HollyWooController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("Movies")
+@RequestMapping("HollyWoo")
 public class HollyWooController {
 
     private static final Logger L = LogManager.getLogger(HollyWooController.class);
