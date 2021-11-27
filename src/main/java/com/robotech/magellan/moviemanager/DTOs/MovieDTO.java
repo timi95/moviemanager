ffee39c5ac7c98afb04749e1f05de410880ca6e2
Timi <timi.ogunkeye@gmail.com>
2021-11-27 15:22:56 +0000

    dto

--- src/main/java/com/robotech/magellan/moviemanager/DTOs/MovieDTO.java
+++ src/main/java/com/robotech/magellan/moviemanager/DTOs/MovieDTO.java
@@ -1,4 +1,12 @@
 package com.robotech.magellan.moviemanager.DTOs;
 
+import com.robotech.magellan.moviemanager.models.Director;
+import com.robotech.magellan.moviemanager.models.Rating;
+
+import java.util.List;
+
 public class MovieDTO {
+    public String name;
+    public List<Rating> ratingsList;
+    public Director director;
 }
