9553d93bb7d8cc48013a901dc6d8ce8e73997b5f
Timi <timi.ogunkeye@gmail.com>
2021-11-28 22:26:08 +0000

    derived queries

--- src/main/java/com/robotech/magellan/moviemanager/repositories/MovieRepository.java
+++ src/main/java/com/robotech/magellan/moviemanager/repositories/MovieRepository.java
@@ -2,10 +2,15 @@ package com.robotech.magellan.moviemanager.repositories;
 
 import com.robotech.magellan.moviemanager.models.Director;
 import com.robotech.magellan.moviemanager.models.Movie;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
+    List<Movie> findByDirectorName(String name);
+
 }
