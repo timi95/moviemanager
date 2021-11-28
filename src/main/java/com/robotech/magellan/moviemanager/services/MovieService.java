32625ab84315162dd012da4db5778e66218ff453
Timi <timi.ogunkeye@gmail.com>
2021-11-28 22:27:25 +0000

    special service functions

--- src/main/java/com/robotech/magellan/moviemanager/services/MovieService.java
+++ src/main/java/com/robotech/magellan/moviemanager/services/MovieService.java
@@ -2,29 +2,38 @@ package com.robotech.magellan.moviemanager.services;
 
 import com.robotech.magellan.moviemanager.DTOs.DirectorDTO;
 import com.robotech.magellan.moviemanager.DTOs.MovieDTO;
+import com.robotech.magellan.moviemanager.models.Director;
 import com.robotech.magellan.moviemanager.models.Movie;
 import com.robotech.magellan.moviemanager.models.Rating;
+import com.robotech.magellan.moviemanager.repositories.DirectorRepository;
 import com.robotech.magellan.moviemanager.repositories.MovieRepository;
+import com.robotech.magellan.moviemanager.repositories.RatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
+    @Autowired
+    private DirectorRepository directorRepository;
+    @Autowired
+    private RatingRepository ratingRepository;
 
     /*CRUD*/
-    private List<Movie> getMovies(){
+    public List<Movie> getMovies(){
         return movieRepository.findAll();
     }
 
-    private Movie findMovie(Long id){
+    public Movie findMovie(Long id){
         return movieRepository.findById(id).get();
     }
 
-    private Movie createMovie(MovieDTO movieDTO){
+    public Movie createMovie(MovieDTO movieDTO){
         return movieRepository.save(
                 new Movie(
                         movieDTO.name,
@@ -32,7 +41,7 @@ public class MovieService {
                         movieDTO.director));
     }
 
-    private Movie updateMovie(Long id, MovieDTO movieDTO){
+    public Movie updateMovie(Long id, MovieDTO movieDTO){
         Movie movie = movieRepository.findById(id).get();
         movie.setName(movieDTO.name);
         movie.setRatings(movieDTO.ratingsList);
@@ -40,7 +49,7 @@ public class MovieService {
         return movieRepository.save(movie);
     }
 
-    private Movie addRatingToMovie(Long id, Rating rating){
+    public Movie addRatingToMovie(Long id, Rating rating){
         Movie movie = movieRepository.findById(id).get();
         List<Rating> ratingsUpdatedList = movie.getRatings();
         ratingsUpdatedList.add(rating);
@@ -48,7 +57,7 @@ public class MovieService {
         return  movieRepository.save(movie);
     }
 
-    private void deleteMovie(Long id){
+    public void deleteMovie(Long id){
         movieRepository.deleteById(id);
     }
 
@@ -56,11 +65,15 @@ public class MovieService {
     * Search movies by Director
     • Search movies where the rating is above a provided rating. */
 
-    private List<Movie> findByDirector(DirectorDTO directorDTO){
-        return null;
+    public List<Movie> findByDirector(String directorName){
+        return movieRepository.findByDirectorName(directorName);
     }
 
-    private List<Movie> findMoviesAboveRating(Long rating){
-        return null;
+    public List<Movie> findMoviesWithRatingHigerThan(Long rating){
+        List<Movie> movies = new ArrayList<>();
+        ratingRepository
+                .findByRatingValueGreaterThan(rating)
+                .forEach(r -> movies.add(r.getMovie()));
+        return movies;
     }
 }
