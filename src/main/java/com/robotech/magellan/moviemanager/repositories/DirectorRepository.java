84c7746da88fd73a563357a51152d3dc4b156b13
Timi <timi.ogunkeye@gmail.com>
2021-12-01 00:01:42 +0000

    repository updates

--- src/main/java/com/robotech/magellan/moviemanager/repositories/DirectorRepository.java
+++ src/main/java/com/robotech/magellan/moviemanager/repositories/DirectorRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DirectorRepository extends JpaRepository<Director, Long>, JpaSpecificationExecutor<Director> {
-    List<Director> findByName(String name);
 }
