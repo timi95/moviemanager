5b010cc1be8b4066ff06b23d453a505683adc458
Timi <timi.ogunkeye@gmail.com>
2021-12-01 00:54:35 +0000

    findByName method, dunno why it dissappeared

--- src/main/java/com/robotech/magellan/moviemanager/repositories/DirectorRepository.java
+++ src/main/java/com/robotech/magellan/moviemanager/repositories/DirectorRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DirectorRepository extends JpaRepository<Director, Long>, JpaSpecificationExecutor<Director> {
+    List<Director> findByName(String name);
 }
